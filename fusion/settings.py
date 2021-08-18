3485e5a67f6a373c597d430cdcb0724f5e5a3038
Joacil Amarante <joacil.amarante@gmail.com>
2021-08-18 13:29:02 -0400

    projeto finalizado3

--- fusion/settings.py
+++ fusion/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nn1cnr#t=ljmnxta0j3zs#tl8-+$0jj6xyx+mbq&61&i&1)m-!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
